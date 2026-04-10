a5c2c3a270f97f8eae18ce3ad7169d546d74b501
musukur <126309190+musukur@users.noreply.github.com>
2026-04-11 02:14:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+// simple test route
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "API is working"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
