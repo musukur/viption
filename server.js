ad7d0a4d57c7bdc5ab2ab4bb896d840d45b7fd09
musukur <126309190+musukur@users.noreply.github.com>
2026-04-24 01:09:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -293,6 +293,8 @@ app.post("/render-ass", async (req, res) => {
 
 /* ============================= */
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
